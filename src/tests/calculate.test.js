07e3890033e8b5b81fc09c7bebdadcde1e56528b
David Robins <david@dlrobins.co.uk>
2019-05-10 16:37:56 +0100

    Fails decimal calculation tests

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,6 +1,6 @@
 import calculate from '../processors/calculate';
 
-describe('calculate function', () => {
+describe('calculate function: positive integers', () => {
     it('adds two positive integers correctly', () => {
         const data = {
             n1: 2,
@@ -39,5 +39,89 @@ describe('calculate function', () => {
         }
         const result = calculate(data);
         expect(result).toEqual(3)
+    });
+})
+
+describe('calculate function: negative integers', () => {
+    it('adds two positive integers correctly', () => {
+        const data = {
+            n1: -2,
+            n2: -3,
+            op: '+'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(-5)
+    });
+
+    it('subtracts two positive integers correctly', () => {
+        const data = {
+            n1: -4,
+            n2: -3,
+            op: '-'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(-1)
+    });
+    
+    it('multiplies two positive integers correctly', () => {
+        const data = {
+            n1: -2,
+            n2: -3,
+            op: '*'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(6)
+    });
+
+    it('divides two positive integers correctly', () => {
+        const data = {
+            n1: -9,
+            n2: -3,
+            op: '/'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(3)
+    });
+})
+
+describe('calculate function: decimals', () => {
+    it('adds two positive integers correctly', () => {
+        const data = {
+            n1: 2.5,
+            n2: 3.5,
+            op: '+'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(6)
+    });
+
+    it('subtracts two positive integers correctly', () => {
+        const data = {
+            n1: 4.9,
+            n2: 3.1,
+            op: '-'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(1.8)
+    });
+    
+    it('multiplies two positive integers correctly', () => {
+        const data = {
+            n1: 2.7,
+            n2: 3.1,
+            op: '*'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(8.37)
+    });
+
+    it('divides two positive integers correctly', () => {
+        const data = {
+            n1: 9.7,
+            n2: 3.2,
+            op: '/'
+        }
+        const result = calculate(data);
+        expect(result).toEqual(3.03125)
     });
 })
